2a0c03f632e89a6c04466652446dc84315b00c28
BiancaBarrosGithub <146129798+BiancaBarrosGithub@users.noreply.github.com>
2023-11-28 10:51:55 -0300

    ClienteF

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesModule } from '../app/clientes/clientes.module';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:''},
+  {path:'clientes', loadChildren:()=> ClientesModule}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
